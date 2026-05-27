08c07cc9ced928c9dcd377b7ffd129d5818a5e55
Ociel Gallardo <ociel.galla@gmail.com>
2026-05-27 01:00:12 +0000

    calculadora

--- Ejercicios/Calculadora/app.js
+++ Ejercicios/Calculadora/app.js
@@ -1,1 +1,41 @@
-const 
+/*aca capturo los botones y el display(input) */
+const display = document.getElementById("display");
+const addNumbers = document.querySelectorAll(".number-btn");
+const addOperators = document.querySelectorAll(".operator-btn");
+const result = document.querySelector(".equals-btn")
+const clear = document.querySelector(".clear-btn")
+
+/*
+LOGICA
+1. Guardar botones en una variable
+2. Recorrer botones
+3. Escuchar clicks
+4. Obtener texto del botón
+5. Mostrarlo en el input 
+*/
+
+addNumbers.forEach(number => {
+    number.addEventListener("click", () => {
+        display.value += number.textContent;
+    })
+})
+
+let firstNumber = ("");
+let operator = ("");
+let secondNumber = ("");
+
+        if (firstNumber) {
+            return firstNumber;
+        } else if (operator) {
+            return secondNumber;
+        }
+addOperators.forEach(operator => {
+    operator.addEventListener("click", () => {
+       
+        display.value  
+    })
+})
+
+clear.addEventListener("click", () => {
+    display.value = ("");
+});
