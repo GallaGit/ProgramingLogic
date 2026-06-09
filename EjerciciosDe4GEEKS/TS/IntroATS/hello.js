67732674a876dcdb06d1068e1406c9585d038ce2
Ociel Gallardo <ociel.galla@gmail.com>
2026-06-09 13:17:08 +0000

    Practica

--- EjerciciosDe4GEEKS/TS/IntroATS/hello.js
+++ EjerciciosDe4GEEKS/TS/IntroATS/hello.js
@@ -1,3 +1,43 @@
 "use strict";
-const greetings = "Hola, TyoeScript!";
-console.log(greetings);
+/*const greetings: string = "Hola, TyoeScript!";
+const year: number = 2026;
+const isLearning: boolean = true;
+
+console.log(greetings)
+console.log("Año: ", year);
+console.log("Estoy aprendiendo?", isLearning)
+
+console.log("-------------------------------");
+
+const nombreUsuario: string = "Maria";
+console.log("Nombre de Usuario:", nombreUsuario);  // Salida: Nombre de Usuario: Maria
+
+// Variable numérica
+let edadUsuario: number = 28;
+console.log("Edad del Usuario:", edadUsuario);  // Salida: Edad del Usuario: 28
+
+// Variable booleana
+const esEstudiante: boolean = true;
+console.log("Es Estudiante:", esEstudiante);  // Salida: Es Estudiante: true
+
+console.log("-------------------------------");
+
+let colorFavorito: string;
+colorFavorito = "azul";
+console.log("Color Favorito:", colorFavorito);
+
+console.log("-------------------------------");
+
+const profileName: string = "Ociel Gallardo";
+const age: number = 35;
+const isMember: boolean = true;
+
+console.log("Name: ", profileName);
+console.log(" Age: ", age)
+console.log("Have a membership: ", isMember);*/
+console.log("-------------------------------");
+const postAuthor = "Alice";
+let postLikes = 0;
+const isPostPublic = true;
+postLikes += 1;
+console.log(`Publicación de ${postAuthor} tiene ${postLikes} likes. Público: ${isPostPublic}`);
--- EjerciciosDe4GEEKS/TS/IntroATS/hello.ts
+++ EjerciciosDe4GEEKS/TS/IntroATS/hello.ts
@@ -1,2 +1,56 @@
-const greetings: string = "Hola, TyoeScript!";
+/*const greetings: string = "Hola, TyoeScript!";
+const year: number = 2026;
+const isLearning: boolean = true;
+
 console.log(greetings) 
+console.log("Año: ", year);
+console.log("Estoy aprendiendo?", isLearning)
+
+console.log("-------------------------------");
+
+const nombreUsuario: string = "Maria";
+console.log("Nombre de Usuario:", nombreUsuario);  // Salida: Nombre de Usuario: Maria
+
+// Variable numérica
+let edadUsuario: number = 28;
+console.log("Edad del Usuario:", edadUsuario);  // Salida: Edad del Usuario: 28
+
+// Variable booleana
+const esEstudiante: boolean = true;
+console.log("Es Estudiante:", esEstudiante);  // Salida: Es Estudiante: true
+
+console.log("-------------------------------");
+
+let colorFavorito: string;
+colorFavorito = "azul";
+console.log("Color Favorito:", colorFavorito);
+
+console.log("-------------------------------");
+
+const profileName: string = "Ociel Gallardo";
+const age: number = 35;
+const isMember: boolean = true;
+
+console.log("Name: ", profileName);
+console.log(" Age: ", age)
+console.log("Have a membership: ", isMember);
+
+console.log("-------------------------------");
+
+const postAuthor: string = "Alice";
+let postLikes: number = 0;
+const isPostPublic: boolean = true;
+
+postLikes += 1;
+
+console.log(`Publicación de ${postAuthor} tiene ${postLikes} likes. Público: ${isPostPublic}`);*/
+//este console.log tambien se puede usar en js
+
+
+//Tipos de datos primitivos en TypeScript
+const age: number = 30;
+const isStudent: boolean = false;
+const name: string = "Alice"; //se puede usar con "... o con '...' o con `...`"
+const uniqueId: symbol = Symbol("id");
+let data: null = null;
+let notAssigned: undefined;
