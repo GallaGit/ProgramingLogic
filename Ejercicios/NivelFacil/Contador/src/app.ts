efee5d875753def094f0ef8aa875ff21bb112d2c
Ociel Gallardo <ociel.galla@gmail.com>
2026-06-11 13:29:23 +0000

    comenzando TS para el contador

---
+++ Ejercicios/NivelFacil/Contador/src/app.ts
@@ -0,0 +1,4 @@
+const counter = document.getElementById("counter");
+const increaseBtn = document.getElementById("increaseBtn");
+const ResetBtn = document.getElementById("ResetBtn");
+let displayNum: number = 0;
