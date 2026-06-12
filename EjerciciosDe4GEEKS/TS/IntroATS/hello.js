c692a062881aad1a6d95a3e636cd914e1c61c85a
Ociel Gallardo <ociel.galla@gmail.com>
2026-06-12 15:25:47 +0000

    estudiando operadores logicos

--- EjerciciosDe4GEEKS/TS/IntroATS/hello.js
+++ EjerciciosDe4GEEKS/TS/IntroATS/hello.js
@@ -34,10 +34,75 @@ const isMember: boolean = true;
 
 console.log("Name: ", profileName);
 console.log(" Age: ", age)
-console.log("Have a membership: ", isMember);*/
+console.log("Have a membership: ", isMember);
+
 console.log("-------------------------------");
-const postAuthor = "Alice";
-let postLikes = 0;
-const isPostPublic = true;
+
+const postAuthor: string = "Alice";
+let postLikes: number = 0;
+const isPostPublic: boolean = true;
+
 postLikes += 1;
+
 console.log(`Publicación de ${postAuthor} tiene ${postLikes} likes. Público: ${isPostPublic}`);
+//este console.log tambien se puede usar en js
+
+
+//Tipos de datos primitivos en TypeScript
+const age: number = 30;
+const isStudent: boolean = false;
+const myName: string = "Alice"; //se puede usar con "... o con '...' o con `...`"
+const uniqueId: symbol = Symbol("id");
+let data: null = null;
+let notAssigned: undefined;*/
+//operadores de comparación para tomar decisiones en el código
+//Operadores de comparación
+/* Igualdad y Desigualdad:
+== (igualdad débil)
+=== (igualdad estricta)
+!= (desigualdad débil)
+!== (desigualdad estricta)
+*/
+//diferencia entre == y ===
+/*const num: number = 5;
+console.log(num=="5");  // true, porque == realiza una comparación de igualdad débil (coerción de tipos)
+console.log(num==="5"); // false, porque === realiza una comparación de igualdad estricta (sin coerción de tipos)
+
+//Operadores de relacionales
+/*
+> (mayor que)
+< (menor que)
+>= (mayor o igual que)
+<= (menor o igual que)
+estos se usan para crear exprosiones booleanas que controlan el flujo del programa, como en condicionales o bucles.
+*/
+/*const edad: number = 25;
+if (edad >= 18) {
+    console.log("Eres mayor de edad.");
+} else {
+    console.log("Eres menor de edad.");
+}   */
+/* Evita encadenar comparaciones como 18 <= edad <= 65. Esto no funciona como se espera en TypeScript.
+En su lugar, usa operadores lógicos para combinar comparaciones:
+*/
+/*if (edad >= 18 && edad <= 65) {
+    console.log("Edad Laboral.");
+}
+*/
+//Operadores logicos
+/*
+&& (AND lógico): Devuelve true si ambos operandos son verdaderos.
+|| (OR lógico): Devuelve true si al menos uno de los operandos es verdadero.
+! (NOT lógico): Invierte el valor de verdad del operando.
+*/
+const isLoggedIn = true;
+const hasNotifications = false;
+const showNotificationBadge = isLoggedIn && hasNotifications;
+console.log(showNotificationBadge); // false
+const isFriend = false;
+const isFollowing = true;
+const canViewStory = isFriend || isFollowing;
+console.log(canViewStory); // true
+const isPrivateAccount = true;
+const canSendMessage = !isPrivateAccount;
+console.log(canSendMessage); // false
--- EjerciciosDe4GEEKS/TS/IntroATS/hello.ts
+++ EjerciciosDe4GEEKS/TS/IntroATS/hello.ts
@@ -43,7 +43,7 @@ const isPostPublic: boolean = true;
 
 postLikes += 1;
 
-console.log(`Publicación de ${postAuthor} tiene ${postLikes} likes. Público: ${isPostPublic}`);*/
+console.log(`Publicación de ${postAuthor} tiene ${postLikes} likes. Público: ${isPostPublic}`);
 //este console.log tambien se puede usar en js
 
 
@@ -53,4 +53,61 @@ const isStudent: boolean = false;
 const myName: string = "Alice"; //se puede usar con "... o con '...' o con `...`"
 const uniqueId: symbol = Symbol("id");
 let data: null = null;
-let notAssigned: undefined;
+let notAssigned: undefined;*/
+
+
+//operadores de comparación para tomar decisiones en el código
+
+//Operadores de comparación
+/* Igualdad y Desigualdad:
+== (igualdad débil)
+=== (igualdad estricta)
+!= (desigualdad débil)
+!== (desigualdad estricta)
+*/
+//diferencia entre == y ===
+/*const num: number = 5;
+console.log(num=="5");  // true, porque == realiza una comparación de igualdad débil (coerción de tipos)
+console.log(num==="5"); // false, porque === realiza una comparación de igualdad estricta (sin coerción de tipos)
+
+//Operadores de relacionales
+/* 
+> (mayor que)
+< (menor que)
+>= (mayor o igual que)
+<= (menor o igual que)
+estos se usan para crear exprosiones booleanas que controlan el flujo del programa, como en condicionales o bucles.
+*/
+/*const edad: number = 25;
+if (edad >= 18) {
+    console.log("Eres mayor de edad.");
+} else {
+    console.log("Eres menor de edad.");
+}   */
+
+/* Evita encadenar comparaciones como 18 <= edad <= 65. Esto no funciona como se espera en TypeScript.
+En su lugar, usa operadores lógicos para combinar comparaciones:
+*/
+/*if (edad >= 18 && edad <= 65) {
+    console.log("Edad Laboral.");
+}
+*/
+//Operadores logicos
+/*
+&& (AND lógico): Devuelve true si ambos operandos son verdaderos.
+|| (OR lógico): Devuelve true si al menos uno de los operandos es verdadero.
+! (NOT lógico): Invierte el valor de verdad del operando.
+*/
+const isLoggedIn  = true;
+const hasNotifications = false;
+const showNotificationBadge = isLoggedIn && hasNotifications;
+console.log(showNotificationBadge); // false
+
+const isFriend = false;
+const isFollowing = true;
+const canViewStory = isFriend || isFollowing;
+console.log(canViewStory); // true
+
+const isPrivateAccount = true;
+const canSendMessage = !isPrivateAccount;
+console.log(canSendMessage); // false
