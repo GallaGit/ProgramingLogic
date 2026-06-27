5fad7b9e13151a653a887f4f4e3599ee9cf66643
Ociel Gallardo <ociel.galla@gmail.com>
2026-06-27 21:52:15 +0200

    piedra papel tijera comenzado

---
+++ Ejercicios/NivelMedio/piedra-papel-tigera/app.js
@@ -0,0 +1,35 @@
+/*const piedraBtn = document.getElementById("piedra");
+const papelBtn = document.getElementById("papel");
+const tijeraBtn = document.getElementById("tijera");
+
+let result = "";*/
+
+/*posibles resultados: 
+🪨 Piedra
+
+📄 Papel
+
+✂️ Tijera
+*/
+
+/*Pseudo
+1- Click en uno de los 3 botones
+2- automaticamente se debe mostrar un random result
+3- condiciones las del uego
+4 mostrar en pantalla result y y tres estados victoria, derrota o empate
+*/
+/*const selection = ["🪨 Piedra" , "📄 Papel" , "✂️ Tijera"]
+
+//jugador
+const playerSelection = "🪨 Piedra";
+
+//PC
+button.addEventListener("click", () => {
+    const randomSelection = Math.floor(Math.random() * selection.length);
+    console.log(playerSelection);
+    console.log(randomSelection);
+})*/
+console.log("Hola");
+
+
+//condiciones del juego
